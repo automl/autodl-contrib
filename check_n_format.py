fbd68b9ee6e415e4381bc0a3dc3c3fb40471b212
ferreirafabio <fabioferreira@mailbox.org>
2020-03-05 01:19:30 +0100

    deal with subdatasets e.g. emnist/mnist

--- check_n_format.py
+++ check_n_format.py
@@ -3,20 +3,8 @@
 
 # Usage: `python3 check_n_format path/to/dataset`
 
-import glob
-import os
 from sys import argv, path
-
-#import dataset_manager
-import pandas as pd
-import yaml
-
-import format_image
-import format_video
-#import run_local_test
-from utils import data_browser
-from utils.image import format_image
-
+import glob, os, yaml
 path.append('utils')
 path.append('utils/image')
 path.append('utils/video')
@@ -24,7 +12,12 @@ STARTING_KIT_DIR = '../autodl/codalab_competition_bundle/autodl_starting_kit_sta
 LOG_FILE = 'baseline_log.txt'
 path.append(STARTING_KIT_DIR)
 path.append(os.path.join(STARTING_KIT_DIR, 'AutoDL_ingestion_program'))
-
+#import dataset_manager
+import pandas as pd
+from utils.image import format_image
+from utils.video import format_video
+#import run_local_test
+from utils import data_browser
 
 def read_metadata(input_dir):
     """ Read private.info with pyyaml
@@ -45,7 +38,7 @@ def count_labels(series):
                 s.add(int(e))
         else:
             s.add(line)
-    s = {x for x in s if x == x}  # remove NaN (example without label)
+    s = {x for x in s if x==x} # remove NaN (example without label)
     return len(s)
 
 
@@ -61,10 +54,8 @@ def compute_stats(labels_df, label_name=None):
     else:
         raise Exception('No labels found, please check labels.csv file.')
     if label_name is not None:
-        if (label_name.shape[0] != res['label_num']):
-            raise Exception(
-                'Number of labels found in label.name and computed manually is not the same: {} != {}'
-                .format(label_name.shape[0], res['label_num']))
+        if(label_name.shape[0] != res['label_num']):
+            raise Exception('Number of labels found in label.name and computed manually is not the same: {} != {}'.format(label_name.shape[0], res['label_num']))
     res['domain'] = 'image'
     return res
 
@@ -81,20 +72,13 @@ def write_info(info_file, res):
 def find_file(input_dir, name):
     """ Find filename containing 'name'
     """
-    filename = [
-        file
-        for file in glob.glob(os.path.join(input_dir, '*{}*'.format(name)))
-    ]
+    filename = [file for file in glob.glob(os.path.join(input_dir, '*{}*'.format(name)))]
     return filename[0]
 
 
 # This are the 3 main functions: format, baseline and check
 
-
-def format_data(input_dir,
-                output_dir,
-                fake_name,
-                effective_sample_num,
+def format_data(input_dir, output_dir, fake_name, effective_sample_num,
                 train_size=0.8,
                 num_channels=3,
                 classes_list=None,
@@ -104,17 +88,13 @@ def format_data(input_dir,
     print('format_data: Formatting... {} samples'.format(effective_sample_num))
     if effective_sample_num != 0:
         if domain == 'image':
-            format_image.format_data(input_dir,
-                                     output_dir,
-                                     fake_name,
+            format_image.format_data(input_dir, output_dir, fake_name,
                                      train_size=train_size,
                                      max_num_examples=effective_sample_num,
                                      num_channels=num_channels,
                                      classes_list=classes_list)
         elif domain == 'video':
-            format_video.format_data(input_dir,
-                                     output_dir,
-                                     fake_name,
+            format_video.format_data(input_dir, output_dir, fake_name,
                                      train_size=train_size,
                                      max_num_examples=effective_sample_num,
                                      num_channels=num_channels,
@@ -133,9 +113,7 @@ def run_baseline(data_dir, code_dir):
 
 def manual_check(data_dir, num_examples=5):
     print('manual_check: Checking manually...')
-    print(
-        'Samples of the dataset are going to be displayed. Please check that the display is correct. Click on the cross after looking at the images.'
-    )
+    print('Samples of the dataset are going to be displayed. Please check that the display is correct. Click on the cross after looking at the images.')
     data_browser.show_examples(data_dir, num_examples=num_examples)
     print('manual_check: done.')
     # TODO: ask for check
@@ -154,8 +132,7 @@ def format_automatically(input_dir, train_size, num_channels):
     metadata = read_metadata(input_dir)
     fake_name = metadata['name']
     print('\nDataset fake name: {}\n'.format(fake_name))
-    labels_df = format_image.get_labels_df(
-        input_dir)  # same function in format_video
+    labels_df = format_image.get_labels_df(input_dir) # same function in format_video
 
     print('First rows of labels file:')
     print(labels_df.head())
@@ -176,12 +153,10 @@ def format_automatically(input_dir, train_size, num_channels):
     print()
 
     # Ask user what he wants to be done
-    effective_sample_num = res[
-        'sample_num']  # if quick check, it'll be the number of examples to format for each class
+    effective_sample_num = res['sample_num'] # if quick check, it'll be the number of examples to format for each class
 
     if not is_formatted(output_dir):
-        print('No formatted version found, creating {} folder.'.format(
-            output_dir))
+        print('No formatted version found, creating {} folder.'.format(output_dir))
         os.mkdir(output_dir)
 
     # booleans
@@ -197,20 +172,12 @@ def format_automatically(input_dir, train_size, num_channels):
         flat_label_list = [item for sublist in label_list for item in sublist]
     print(flat_label_list)
     domain = 'image'
-    format_data(input_dir,
-                output_dir,
-                fake_name,
-                effective_sample_num,
-                train_size=train_size,
-                num_channels=num_channels,
-                classes_list=flat_label_list,
-                domain=domain)
+    format_data(input_dir, output_dir, fake_name, effective_sample_num, train_size=train_size, num_channels=num_channels, classes_list=flat_label_list, domain=domain)
     formatted_dataset_path = os.path.join(output_dir, fake_name)
 
     # run baseline
     if do_run_baseline:
-        code_dir = os.path.join(STARTING_KIT_DIR,
-                                'AutoDL_sample_code_submission')
+        code_dir = os.path.join(STARTING_KIT_DIR, 'AutoDL_sample_code_submission')
         run_baseline(formatted_dataset_path, code_dir)
         # TODO: save results in log file
 
@@ -224,24 +191,21 @@ def format_automatically(input_dir, train_size, num_channels):
     write_info(public_info_file, res)
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
 
-    if len(argv) == 2:
+    if len(argv)==2:
         input_dir = argv[1]
         input_dir = os.path.normpath(input_dir)
         output_dir = input_dir + '_formatted'
     else:
-        print(
-            'Please enter a dataset directory. Usage: `python3 check_n_format path/to/dataset`'
-        )
+        print('Please enter a dataset directory. Usage: `python3 check_n_format path/to/dataset`')
         exit()
 
     # Read the meta-data in private.info.
     metadata = read_metadata(input_dir)
     fake_name = metadata['name']
     print('\nDataset fake name: {}\n'.format(fake_name))
-    labels_df = format_image.get_labels_df(
-        input_dir)  # same function in format_video
+    labels_df = format_image.get_labels_df(input_dir) # same function in format_video
 
     print('First rows of labels file:')
     print(labels_df.head())
@@ -262,28 +226,21 @@ if __name__ == "__main__":
     print()
 
     # Ask user what he wants to be done
-    effective_sample_num = res[
-        'sample_num']  # if quick check, it'll be the number of examples to format for each class
+    effective_sample_num = res['sample_num'] # if quick check, it'll be the number of examples to format for each class
 
-    quick_check = 1  # just for display purpose
+    quick_check = 1 # just for display purpose
     if not input('Quick check? [Y/n] ') in ['n', 'N']:
         # quick check
-        print(
-            'Quick check enabled: running script on a small subset of data to check if everything works as it should.'
-        )
+        print('Quick check enabled: running script on a small subset of data to check if everything works as it should.')
         output_dir = output_dir + '_mini'
         effective_sample_num = min(effective_sample_num, 1)
-        quick_check = res['label_num']  # just for display purpose
+        quick_check = res['label_num'] # just for display purpose
 
     if is_formatted(output_dir):
         # Already exists
-        if not input('Overwrite existing formatted data? [Y/n] ') in [
-                'n', 'N'
-        ]:
+        if not input('Overwrite existing formatted data? [Y/n] ') in ['n', 'N']:
             # Overwrite
-            if input('Re-format all {} files? [Y/n] '.format(
-                    effective_sample_num * quick_check)) in ['n', 'N'
-                                                             ]:  # Confirmation
+            if input('Re-format all {} files? [Y/n] '.format(effective_sample_num * quick_check)) in ['n', 'N']: # Confirmation
                 # Do nothing
                 exit()
         else:
@@ -291,8 +248,7 @@ if __name__ == "__main__":
 
     # Init output_dir
     else:
-        print('No formatted version found, creating {} folder.'.format(
-            output_dir))
+        print('No formatted version found, creating {} folder.'.format(output_dir))
         os.mkdir(output_dir)
 
     # domain (image, video, text, etc.)
@@ -311,9 +267,7 @@ if __name__ == "__main__":
         exit()
 
     # booleans
-    do_run_baseline = not input('Run baseline on formatted data? [Y/n] ') in [
-        'n', 'N'
-    ]
+    do_run_baseline = not input('Run baseline on formatted data? [Y/n] ') in ['n', 'N']
     do_manual_check = not input('Do manual check? [Y/n] ') in ['n', 'N']
 
     # format data in TFRecords
@@ -324,19 +278,12 @@ if __name__ == "__main__":
         label_list = label_name.values.tolist()
         flat_label_list = [item for sublist in label_list for item in sublist]
     print(flat_label_list)
-    format_data(input_dir,
-                output_dir,
-                fake_name,
-                effective_sample_num,
-                num_channels=num_channels,
-                classes_list=flat_label_list,
-                domain=domain)
+    format_data(input_dir, output_dir, fake_name, effective_sample_num, num_channels=num_channels, classes_list=flat_label_list, domain=domain)
     formatted_dataset_path = os.path.join(output_dir, fake_name)
 
     # run baseline
     if do_run_baseline:
-        code_dir = os.path.join(STARTING_KIT_DIR,
-                                'AutoDL_sample_code_submission')
+        code_dir = os.path.join(STARTING_KIT_DIR, 'AutoDL_sample_code_submission')
         run_baseline(formatted_dataset_path, code_dir)
         # TODO: save results in log file
 
